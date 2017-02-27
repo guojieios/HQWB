b8adbd441dfa5ba5a62349f2a6e3a4909f527f36
guojieios <531829791@qq.com>
2017-02-27 18:39:46 +0800

    纯代码初始化项目
    纯代码初始化项目

---
+++ HQWB/HQWB/Classes/Main(主要)/MainViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MainViewController.swift
+//  HQWB
+//
+//  Created by Jerry_hqyj on 17/2/27.
+//  Copyright © 2017年 Jerry_hqyj. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+       
+        
+        
+        // 调用方法，传入参数
+       
+        addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home", SelectImageName: "tabbar_home_highlighted")
+        addChildViewController(DiscoverViewController(), title: "发现", imageName: "tabbar_discover", SelectImageName: "tabbar_discover_highlighted")
+        addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center", SelectImageName: "tabbar_message_center_highlighted")
+        addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile", SelectImageName: "tabbar_profile_highlighted")
+        
+        
+      
+        
+        
+    }
+
+   
+    // Swift中 支持方法的重载的 ----  OC中不支持
+    // 方法的重载 : 方法名相同，但是参数不同 --- 1.参数类型不同 2. 参数个数不同
+    // 方法不能随便调用 ----  变成私有方法
+    private func addChildViewController(childVc: UIViewController,title : String,imageName : String,SelectImageName : String) {
+        
+        
+        
+        // 设置子控制器的属性
+        childVc.title = title
+        childVc.tabBarItem.image = UIImage(named: imageName)
+        childVc.tabBarItem.selectedImage = UIImage(named: SelectImageName)
+        
+        // 设置导航控制器
+        let childNav = UINavigationController(rootViewController: childVc)
+        
+        
+        // 添加导航控制器
+        addChildViewController(childNav)
+        
+        
+        
+    }
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
