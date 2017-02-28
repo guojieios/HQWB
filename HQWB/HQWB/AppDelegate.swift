e0d7b5806cc4966a7ca622cdd39b8508e1e85ea0
guojieios <531829791@qq.com>
2017-02-28 19:25:36 +0800

    storyboard reference的使用
    修改项目的框架，用storyboard创建

--- HQWB/HQWB/AppDelegate.swift
+++ HQWB/HQWB/AppDelegate.swift
@@ -20,17 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // appearance - 统一设置
         UITabBar.appearance().tintColor = UIColor.orangeColor()
         
-        // 1.创建Window
-        
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        // 2. 给window一个根控制器
-        window?.rootViewController = MainViewController()
-        // 3.把window设置可见
-        window?.makeKeyAndVisible()
-        
-        
-//        MainViewController().addChildViewController(<#T##childVc: UIViewController##UIViewController#>, title: <#T##String#>, imageName: <#T##String#>, SelectImageName: <#T##String#>)
-        
         
         
         return true
