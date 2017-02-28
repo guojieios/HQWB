16c816a15ab3c355abd68144d93ecc22cf474927
guojieios <531829791@qq.com>
2017-02-28 20:48:31 +0800

    调整tabbar的items
    代码设置tabbar的items，并且添加发布按钮

--- HQWB/HQWB/Classes/Main(主要)/MainViewController.swift
+++ HQWB/HQWB/Classes/Main(主要)/MainViewController.swift
@@ -10,11 +10,76 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
+    // 需要字符串数组存取 图片名称
+    // Swift 中懒加载
+   private lazy var imageNames = ["tabbar_home","tabbar_message_center","","tabbar_discover","tabbar_profile"]
+    
+   private lazy var composeButton = UIButton()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+       // 设置 发布按钮
+        // 1. 添加到视图中
+        tabBar.addSubview(composeButton)
+        
+        // 2. 设置图片
+        composeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        composeButton.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: .Highlighted)
+        composeButton.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: .Normal)
+        composeButton.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: .Highlighted)
+        
+        // 设置大小
+        composeButton.sizeToFit()
+        
+        
+        // 3. 设置位置
+        composeButton.center = CGPointMake(tabBar.center.x, tabBar.bounds.size.height * 0.5)
        
-     }
+        
+        
+        }
+        
+    
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        // 用代码设置 items 的选中图片
+        
+        // 1.遍历items
+        for i in 0..<tabBar.items!.count {
+            
+            
+            // 2.拿到item
+            let item = tabBar.items![i]
+            
+            // 设置中间按钮 不与用户交互
+            if(i == 2) {
+                
+                item.enabled = false
+                
+                continue
+                
+            }
+            
+            
+            // 3.设置高亮状态
+            
+            item.selectedImage = UIImage(named: imageNames[i] + "_highlighted")
+            
+            
+
+        
+        
+        
+    }
+    }
+    
  
     
 }
