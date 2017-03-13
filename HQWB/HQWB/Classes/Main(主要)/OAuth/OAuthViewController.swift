1e6457bcb9877f6ffdb071acbb9348fee93da553
guojieios <531829791@qq.com>
2017-03-13 14:55:13 +0800

    OAuth认证获取到code
    用Appkey与回调地址获取到code

--- HQWB/HQWB/Classes/Main(主要)/OAuth/OAuthViewController.swift
+++ HQWB/HQWB/Classes/Main(主要)/OAuth/OAuthViewController.swift
@@ -9,27 +9,101 @@
 import UIKit
 
 class OAuthViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+        //        https://api.weibo.com/oauth2/authorize?client_id=453636876&redirect_uri=http://www.makeru.com.cn/
+        
+        
+        
+        SetUpNavItem()
+        
+        
+        
+        loadPage()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+// UI设置
+extension OAuthViewController {
+    
+    
+    func SetUpNavItem() {
+        
+        // 1.设置左侧的item
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: .Plain, target: self, action: "leftButtonClick")
+        
+        
+        // 2.设置右边的item
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "填充", style: .Plain, target: self, action: "rightButtonClick")
+        
+        
+        
+        // 3.设置标题
+        title = "登录"
+        
+        
+    }
+    
+    
+    
+    func loadPage() {
+        
+        //  字符串链接
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(app_key)&redirect_uri=\(redirect_uri)"
+        
+        // 链接
+        guard let url = NSURL(string: urlString) else {
+            
+            return
+        }
+        
+        
+        
+        // 请求
+        let request = NSURLRequest(URL: url)
+        
+        // 加载网页
+        webView.loadRequest(request)
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
+
+// 点击事件
+extension OAuthViewController {
+    
+    @objc private func leftButtonClick() {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        
+    }
+    
+    
+    @objc private func rightButtonClick() {
+        
+        print("填充")
+        
+        
+    }
+    
+    
+}
+
+
+
---
+++ HQWB/HQWB/Classes/Tools(工具)/Commend.swift
@@ -0,0 +1,19 @@
+//
+//  Commend.swift
+//  HQWB
+//
+//  Created by Jerry_hqyj on 17/3/13.
+//  Copyright © 2017年 Jerry_hqyj. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+// 设置常量
+let app_key = "453636876"
+let redirect_uri = "http://www.makeru.com.cn/"
+let App_Secret = "c82a1280e93bd9abfd1459d4062194a9"
+        
